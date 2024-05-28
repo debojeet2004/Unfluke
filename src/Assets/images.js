dc04241f562e6a62f2232e93f62aaa12c504909a
Debojeet Karmakar <debojeetkarmakar2020@gmail.com>
2024-05-28 22:15:29 +0530

    nav chnages

---
+++ src/Assets/images.js
@@ -0,0 +1,10 @@
+import bell from './bell.png';
+import coin from './coin.png';
+import man from './man.png';
+
+
+export {
+    bell,
+    coin,
+    man,
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
-
+import { coin ,bell,man } from "../../Assets/images"
 
 function Navbar() {
     return (
         <div className=" w-full p-2 flex justify-between items-center">
             <div className=" p-1 px-2 font-semibold md:text-2xl lg:text-3xl">Unfluke</div>
             <div className=" flex gap-6">
-            <div className="w-8 h-8 border flex justify-center items-center rounded-md" >@</div>
-            <div className="w-8 h-8 border flex justify-center items-center rounded-md" >@</div>
-            <div className="w-8 h-8 border flex justify-center items-center rounded-md" >@</div>
-            <div className="w-8 h-8 border flex justify-center items-center rounded-full" >@</div>
+            <img src={coin} alt="" className="w-8 h-8  flex justify-center items-center rounded-md" />
+            <img src={bell} alt="" className="w-8 h-8  flex justify-center items-center rounded-md" />
+            <div className="w-8 h-8 border flex justify-center items-center rounded-md" ></div>
+            <img src={man} alt="" className="w-8 h-8  flex justify-center items-center rounded-md" />
             </div>
         </div>
     )
